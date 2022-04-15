f4587ca9afee9af0c4961a4da6f8cf4c7925cf06
BJCDevelop <bjcdevelop@gmail.com>
2022-04-15 15:09:45 -0400

    First commit by Braulio Chavez

---
+++ config/default.ts
@@ -0,0 +1,14 @@
+export default {
+    port: 3000,
+    dbUri: "mongodb://localhost:27017/back-datasport",
+    logLevel: "info",
+    accessTokenPrivateKey: "",
+    refreshTokenPrivateKey: "",
+    smtp: {
+        user: "aat2ltgixeyq7pxd@ethereal.email",
+        pass: "3kcf9XeuMWmJPSz8W9",
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+    }
+}
